378e3d3747c46ed5c9cf73599dc3de624ce5cea3
HARSHBHUDOLIA <aivankum@gmail.com>
2020-10-20 17:28:15 +0530

    Build here

---
+++ build/precache-manifest.fc577d0ac263fa19a833606157d32144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c743cf64fa742bd7ff19bdd0fe96998e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e1f33505ebdb65ef255",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "1b14c2f488bcafcb2674",
+    "url": "/static/css/main.67022bf9.chunk.css"
+  },
+  {
+    "revision": "4e1f33505ebdb65ef255",
+    "url": "/static/js/2.f0cb062d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0cb062d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b14c2f488bcafcb2674",
+    "url": "/static/js/main.6271b7ce.chunk.js"
+  },
+  {
+    "revision": "d82bc5f42b63feb20476",
+    "url": "/static/js/runtime-main.35e9da1c.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express')
+
+const path=require('path');
+const port =4000;
+const app=express();
+
+app.use(express.static(path.join(__dirname,'build')))//This will look into the static file from the build folder
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build/index.html'))
+})
+
+app.listen(port,(err)=>{
+  if(err) throw err;
+  console.log('server running on port ');
+})
